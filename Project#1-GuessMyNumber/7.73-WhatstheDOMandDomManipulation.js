1e27d500847b4d39f7b72fe44faeca9b41ed909b
yarfill <fill3272@gmail.com>
2025-02-03 12:53:11 -0500

    Whats the DOM and DOM Manipulation

---
+++ Project#1-GuessMyNumber/7.73-WhatstheDOMandDomManipulation.js
@@ -0,0 +1,8 @@
+document.querySelector('.message').textContent = 'Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 11;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
