d432f2892a62bd308f1a295cd865cf28b129c3eb
yarfill <fill3272@gmail.com>
2025-02-07 17:59:45 -0500

    working with classes

---
+++ Project#2-ModalWindow/lesson-#7.82.js
@@ -0,0 +1,21 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+function openModal() {
+  console.log("Btn clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+for(let i = 0; i < btnsOpenModal.length; i++) 
+  //  !   !   !  \\
+btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
