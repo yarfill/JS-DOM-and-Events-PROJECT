9fa9bdc93dde16247b959f2b9d1cde76c29ae370
yarfill <fill3272@gmail.com>
2025-02-10 09:51:37 -0500

    learning how to handling keypress event

---
+++ Project#2-ModalWindow/lesson-#7.83.js
@@ -0,0 +1,29 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+function openModal() {
+
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+for(let i = 0; i < btnsOpenModal.length; i++) 
+btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+//   !   !   !   \\
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains('hidden')) { // if(!modal.classList.contains('hidden')) closeModal();
+    closeModal();
+  }
+});
--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 'use strict';
-
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-const btnsOpenModal = document.querySelectorAll(".show-modal");
-
-for(let i = 0; i < btnsOpenModal.lenght; i++) 
-  console.log(btnsOpenModal[i].textContent);
