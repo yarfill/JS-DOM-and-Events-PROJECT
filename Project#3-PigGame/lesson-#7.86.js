f0e59982f61c969b65f2184360f98fa60713792a
yarfill <fill3272@gmail.com>
2025-02-11 09:19:23 -0500

    Made functionality Switching Active Player works

--- Project#3-PigGame/lesson-#7.86.js
+++ Project#3-PigGame/lesson-#7.86.js
@@ -1,26 +1,24 @@
-// selecting elements
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1"); // getElementById supposed to be faster than querySelector!
-const currentEl0 = document.getElementById("current--0");
-const currentEl1 = document.getElementById("current--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 
 const name0 = document.querySelector(".name--0");
 const name1 = document.querySelector(".name--1");
-const player0 = document.querySelector(".player--0");
-const player1 = document.querySelector(".player--1");
-
-let currentScore = 0;
-
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-// Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
-
 diceEl.classList.add("hidden");
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener("click", function() {
@@ -35,8 +33,13 @@ btnRoll.addEventListener("click", function() {
   if(dice !== 1) {
     // Add dice to the current score
     currentScore = currentScore + dice; // shortcut currentScore += dice
-    currentEl0.textContent = currentScore; // change it later
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle(`player--active`);
+    player1El.classList.toggle(`player--active`);
   }
 });
